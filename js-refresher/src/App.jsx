2395b6029d6987f63e16e86dadcc4bf86b092314
Zion Koundou <kodesone13@gmail.com>
2026-05-23 09:55:13 -0700

    Swithcing to using tailwind and importing the assets

--- js-refresher/src/App.jsx
+++ js-refresher/src/App.jsx
@@ -1,43 +1,19 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react';
 
-//main ui chills here
-//class components work but its some old jawn
-//they usually use function components nowadays
 const App = () => {
-  
-  // set has liked is used to actual set the state
-  return ( 
-    <div className = "card-container">
-      <Card title="Movies" />
-      <Card title="TV Shows"/>
-      <Card title="Books" />
-    </div>
 
-  )
+    return (
+        <main>
+            <div>
+                <header>
+                    <h1>Find <span className= "text-gradient">Movies</span> You'll Enjoy
+                    Without the Hassle</h1>
+                </header>
+                <p>Search</p>
+            </div>
+            
+        </main>
+    )
 }
 
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  
-  useEffect(() => {
-    console.log(`${title} The card  has been ${hasLiked ? 'liked' : 'unliked'}`);
-  });
-  
-  return (
-    <div className = "card">
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'} 
-      </button>
-    </div>
-  )
-}
-
-//state holds info that changes over time
-//using a regular variable as opposed to a state, it wont work
-// wont update variables that change. relys on state
-export default App
+export default App;
