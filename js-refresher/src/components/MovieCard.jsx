6576705615f7531c97455b7563c4947a1c46c5f8
Zion Koundou <kodesone13@gmail.com>
2026-05-27 15:14:15 -0700

    polishing the movie  card
    - added movie lang, rating, and release year into the card component

--- js-refresher/src/components/MovieCard.jsx
+++ js-refresher/src/components/MovieCard.jsx
@@ -4,10 +4,36 @@ const MovieCard = ({ movie: {title, vote_average, poster_path, release_date, ori
 
     return (
         <div className="movie-card">
-            {/* why must we use the like path like this to get the images. */}
-            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}`: "/no-movie.png"}></img>
-            {/* why do we have key again? */}
-            <p  className="text-white"> {title}</p>
+            
+            <div>
+                {/* why must we use the like path like this to get the images. */}
+                <img src={poster_path ? 
+                        `https://image.tmdb.org/t/p/w500/${poster_path}`: "/no-movie.png"}
+                    alt={title}
+                />
+                {/* why do we have alt again? */}
+            </div>
+            
+            <div className="mt-4 ">
+                 <h3> {title} </h3>
+                 <div className="content">
+                    <div className="rating">
+                        <img src="star.svg" alt="Star Icon"></img>
+                        <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+                        {/* what is the to fixed method from and for */}
+                    </div>
+
+                    <span>•</span>
+
+                    <p className= "lang">{original_language ? original_language : "N/A"}</p>
+
+                    <span>•</span>
+
+                    <p className='year'> {release_date ? release_date.split("-")[0] : "N/A"}</p>
+
+                 </div>
+            </div>
+           
         </div>
     )
 }
